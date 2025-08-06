abdff7e4c81cbe3cf564bcbc077b038324185be8
mac kawara <mkawara23@gmail.com>
2025-08-06 14:07:47 +0200

    feat: assignemnt question
     feat: add assignment question
    
     feat: add assignment question

---
+++ assignment.js
@@ -0,0 +1,9 @@
+/* 
+create  branch named assignment-submission-yourname.
+create an html file called assignment-yourname.html.
+create a JavaScript file named assignment-submission-yourname.js.
+link the JavaScript file to the HTML file.
+Declare two variables: one called firstName and another called lastName.
+ Assign your own first and last name as string values. 
+ Then create a third variable called fullName that combines firstName and lastName with a space in between.
+Finally, log the full name to the console. */
