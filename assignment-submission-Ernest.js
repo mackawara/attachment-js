ac8d2099a0c43c202547ed8b2f6a6807a8e23459
Ernest Javachava <ernestjavaz@gmail.com>
2025-08-07 14:11:25 +0200

    add comments

--- assignment-submission-Ernest.js
+++ assignment-submission-Ernest.js
@@ -1,6 +1,6 @@
 //a program that adds two strings together
-const firstName="Ernest";
-const lastName="Javachava";
-const fullName=firstName+" "+lastName;
+const firstName="Ernest"; // creating a string variable and assingning it with a value called Ernest
+const lastName="Javachava";// creating a string variable and assingning it with a value called Javachava
+const fullName=firstName+" "+lastName; // this variable concatinates 2 strings to together
 
-console.log(fullName);
+console.log(fullName); //the output of the program will be Ernest Javachava
